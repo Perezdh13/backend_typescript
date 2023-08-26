02e4cce9e992ee432da0a9a4c11bd8f23bee6481
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-08-26 10:51:57 +0200

    metodos get creados y probados

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,14 +1,33 @@
-import  express from "express"
-import * as diary from"../services/diary"
+import express from 'express'
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router();
 
-router.get("/",(_req,res) => {
-res.send(diary.getEntriesWithoutSensitiveInfo())
+router.get("/", (_req, res) => {
+    res.send(diaryServices.getEntriesWithoutSensitiveInfo());
 });
 
-router.post("/",(_req,res) => {
-res.send("saving a diary!")
+router.get('/:id', (req, res) => {
+    const diary = diaryServices.findById(+req.params.id)
+
+    return (diary != null)
+        ? res.send(diary)
+        : res.sendStatus(404)
+
 });
+
+router.post("/", (req, res) => {
+    const { date, weather, visibility, comment } = req.body
+
+    const newDiaryEntry = diaryServices.addDiary(
+        date,
+        weather,
+        visibility,
+        comment
+    )
+        res.json(newDiaryEntry)
+
+});
+
 
 export default router;
